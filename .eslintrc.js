191f12fbc81a79a68d1cfd37d59d8f99d6a323f4
singcl <24661881@qq.com>
2018-05-17 11:18:24 +0800

    :art: Esliint with node|browser|es6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,25 @@
+// http://eslint.cn/docs/user-guide/configuring
 module.exports = {
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 8
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true 
+    },
+    "globals": {},
     "extends": "airbnb",
     "rules": {
-        "indent": ["error", 4],
+        "max-len": ["error", { "comments": 200 }],
         "linebreak-style": 0,
+        "indent": ["error", 4],
+        "no-var": "off",
+        "semi": ["error", "always"],
+        "max-len": ["error", 180],
+        "no-multi-spaces": ["error", { ignoreEOLComments: true }],
+        "no-console": 0,
         "prefer-template": "warn",
         "prefer-destructuring": "warn"
     }
