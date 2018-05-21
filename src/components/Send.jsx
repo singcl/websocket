c4dac52ab4ed3c629b4d685a1192ad66e091f0fa
singcl <24661881@qq.com>
2018-05-21 11:38:29 +0800

    :art: Send 发送组件构建完成...

--- src/components/Send.jsx
+++ src/components/Send.jsx
@@ -1,11 +1,42 @@
-/*
- * @Author: singcl
- * @Date: 2018-05-21 09:54:42
- * @Last Modified by:   singcl
- * @Last Modified time: 2018-05-21 09:54:42
- */
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Send = () => (<div>Send</div>);
+const Send = ({
+    handleSend,
+    isDisabledSend,
+    handleChangeSend,
+    typingValue,
+}) => {
+    let textInput = null;
+    return (
+        <form className="send" onSubmit={(e) => handleSend(e, textInput)}>
+            <div className="input-group full-height">
+                <input
+                    type="text"
+                    className="form-control full-height-change"
+                    onChange={handleChangeSend}
+                    value={typingValue}
+                    ref={(input) => textInput = input}
+                />
+                <span className="input-group-btn">
+                    <button
+                        className="btn btn-info full-height-change"
+                        disabled={isDisabledSend}
+                        onClick={(e) => handleSend(e, textInput)}
+                    >
+                        Send
+                    </button>
+                </span>
+            </div>
+        </form>
+    );
+};
+
+Send.propTypes = {
+    handleSend: PropTypes.func.isRequired,
+    handleChangeSend: PropTypes.func.isRequired,
+    isDisabledSend: PropTypes.bool.isRequired,
+    typingValue: PropTypes.string.isRequired,
+};
 
 export default Send;
